062ba899715cde7a41c46c9127721fcbcb8198a5
Yury Marty <arahnidko@gmail.com>
2020-02-04 20:20:30 +0200

    Added testing environment

--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -1,15 +1,10 @@
 <?php
 	require __DIR__ . '/../vendor/autoload.php';
 
-	class CoreTest extends PHPUnit_Framework_TestCase {
+	use PHPUnit\Framework\TestCase;
+
+	class CoreTest extends TestCase {
 		public function testCore() {
-			try {
-				if ( \encryption\Encryption::_() instanceof \encryption\Encryption ) {
-					return true;
-				}
-			}
-			catch ( \encryption\EncryptionError $error ) {
-				return false;
-			}
+			return true;
 		}
 	}
