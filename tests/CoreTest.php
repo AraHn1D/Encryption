17ae29bbb9b2ccd9822b76e5b4c441900da194f0
Yury Marty <arahnidko@gmail.com>
2020-02-11 18:30:12 +0200

    Changes in test

--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -2,9 +2,22 @@
 	require __DIR__ . '/../vendor/autoload.php';
 
 	use PHPUnit\Framework\TestCase;
+	use encryption\Encryption;
+	use encryption\Encrypted;
+	use encryption\Decrypted;
+	use encryption\EncryptionError;
 
 	class CoreTest extends TestCase {
 		public function testCore() {
-			return \encryption\Encryption::_() instanceof \encryption\Encryption;
+			try {
+				$instance = Encryption::_([
+					'mode' => Encryption::SERVER_MODE
+				]);
+			}
+			catch (EncryptionError $error) {
+				return false;
+			}
+
+			return $instance instanceof \encryption\Encryption;
 		}
 	}
